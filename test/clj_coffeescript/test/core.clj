472298f76ce194aa808315416bee66ef1d990a2c
tbatchelli MBP <tbatchelli@acm.org>
2010-11-24 12:21:19 -0800

    Initial commit

---
+++ test/clj_coffeescript/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-coffeescript.test.core
+  (:use [clj-coffeescript.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
