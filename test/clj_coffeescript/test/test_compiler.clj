8d4e7211a2895e1a9c49d36bae036a7ed2af703d
tbatchelli MBP <tbatchelli@acm.org>
2010-11-27 12:21:27 -0800

    Added test to verify evaluation of coffeescript in different scopes works properly.

--- test/clj_coffeescript/test/test_compiler.clj
+++ test/clj_coffeescript/test/test_compiler.clj
@@ -6,10 +6,18 @@
 (deftest end-to-end
   (testing "Building the compiler and compiling one line"
     (is (= (let [compiler (build-compiler)]
-             (compile-string compiler "a=12" true)))
-        "var a;\na = 12;"))
+             (compile-string compiler "a=12" true))
+           "var a;\na = 12;")))
   (testing "compiling and running coffeescript code"
     (is (= (let [compiler (build-compiler)
                  runtime (rhino/build-scope)]
-             (evaluate-string compiler runtime "a=1+3;")))
-        4)))
+             (evaluate-string compiler runtime "a=1+3;"))
+           4)))
+  (testing "evaluating scripts in scopes work"
+    (is (= (let [compiler (build-compiler)
+                 runtime-1 (rhino/build-scope)
+                 runtime-2 (rhino/build-scope)]
+             (evaluate-string compiler runtime-1 "a=1;")
+             (evaluate-string compiler runtime-2 "a=2;")
+             (evaluate-string compiler runtime-1 "b=a*a;"))
+           1))))
